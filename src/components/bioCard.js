09bd0c305790308467a5849ed776e1be181f1d6b
moskowitza <moskowitzaaron@mac.com>
2019-07-30 13:00:59 -0400

    playing around with biocard and about

--- src/components/bioCard.js
+++ src/components/bioCard.js
@@ -1,26 +1,35 @@
 import React from "react"
 import styled from "styled-components"
+import { useSpring, animated } from "react-spring"
 
 const BioWrapper = styled.div`
   font-family: Arial, Helvetica, sans-serif;
   align-items: center;
-  border: 1px solid blue;
+  border-top: 3px solid ${props => props.color};
+  box-shadow: 10px 10px 20px 6px ${props => props.shadow};
+  border-radius: 5px;
   padding: 1rem;
   display: flex;
+  margin: 2rem;
+  h1 {
+  }
   img {
     max-height: 150px;
     margin: 0 2rem 0 1rem;
   }
 `
 
-const BioCard = ({ name, role, image, description }) => (
-  <BioWrapper>
-    <img src={image} alt={name} />
-    <div>
-      <h2>{name}</h2>
-      <h4>{role}</h4>
-      <p>{description}</p>
-    </div>
-  </BioWrapper>
-)
+const BioCard = ({ name, position, image, description, color, shadow }) => {
+  const spring = useSpring({ opacity: 1, from: { opacity: 0 } })
+  return (
+    <BioWrapper color={color} shadow={shadow}>
+      <img src={image} alt={name} />
+      <div>
+        <animated.h2 style={{ ...spring }}>{name}</animated.h2>
+        <h4>{position}</h4>
+        <p>{description}</p>
+      </div>
+    </BioWrapper>
+  )
+}
 export default BioCard
--- src/pages/about.js
+++ src/pages/about.js
@@ -16,13 +16,19 @@ const About = ({ location }) => (
     </p>
     <Link to="/"> Home</Link>
     <BioCard
+      shadow="pink"
+      color="red"
       name="Aaron"
       role="Webmaster & Eater"
       description="I'm a fullstack developer and love working with React and Node and mentoring new developers. When I'm not sampling Philadephia vegan cheese steaks I'm rock-climbing or spending time with my family. Other hobbies include: eating vegan cheese steaks, writing about vegan cheese steaks, photographing vegan cheese steaks, and collecting data on vegan cheese steaks."
       image={aaronsFace}
-    >
-      {/* <AaronImage /> */}
-    </BioCard>
+    />
+    <BioCard
+      name="Aaron"
+      position="Webmaster & Eater"
+      description="I'm a fullstack developer and love working with React and Node and mentoring new developers. When I'm not sampling Philadephia vegan cheese steaks I'm rock-climbing or spending time with my family. Other hobbies include: eating vegan cheese steaks, writing about vegan cheese steaks, photographing vegan cheese steaks, and collecting data on vegan cheese steaks."
+      image={aaronsFace}
+    />
   </Layout>
 )
 
